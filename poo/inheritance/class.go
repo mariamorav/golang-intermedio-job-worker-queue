cc12cfb5ae1034eac6316a1e4f49e1a9f2dc8b67
Maria Mora <mariamora2807@gmail.com>
2022-08-24 14:04:35 -0500

    class: herencia

---
+++ poo/inheritance/class.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+type Employee struct {
+	id int
+}
+
+type FullTimeEmployee struct {
+	Person
+	Employee
+}
+
+func GetMessage(p Person) {
+	fmt.Printf("%s with age %d\n", p.name, p.age)
+}
+
+func main() {
+	ftEmployee := FullTimeEmployee{}
+	ftEmployee.name = "Name"
+	ftEmployee.age = 29
+	ftEmployee.id = 1
+
+	fmt.Printf("%v\n", ftEmployee)
+	//GetMessage(ftEmployee)
+}
---
+++ poo/inheritance/class.ts
@@ -0,0 +1,59 @@
+class BaseEmployee {
+    private id:  number;
+    private name: string;
+    
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public getMessage() : string {
+        return `${this.name} with Id ${this.id}`
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+}
+
+class TemporaryEmployee extends BaseEmployee {
+    private endDate: string;
+
+    constructor(id: number, name: string, endDate: string) {
+        super(id, name);
+        this.endDate = endDate;
+    }
+
+    public getEndDate(): string {
+        return this.endDate;
+    }
+
+    public getMessage(): string {
+        return `${this.getName()} with id: ${this.getId()} and endDate ${this.endDate}`
+    }
+}
+
+class FullTimeEmployee extends BaseEmployee {
+    private taxRate: number;
+
+    constructor(id: number, name: string, taxRate: number) {
+        super(id, name);
+        this.taxRate = taxRate;
+    }
+
+    public getTaxRate(): number {
+        return this.taxRate;
+    }
+
+    public getMessage(): string {
+        return `${this.getName()} with id: ${this.getId()} and taxRate ${this.taxRate}`
+    }
+}
+
+const printGetMessage = (employee: BaseEmployee): void => {
+    console.log(employee.getMessage())
+}
