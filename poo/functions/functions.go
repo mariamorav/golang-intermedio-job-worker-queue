5d9e31908a513ef006306c9cf3697f70b1de4928
Maria Mora <mariamora2807@gmail.com>
2022-08-24 16:55:01 -0500

    class: funciones anonimas

---
+++ poo/functions/functions.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//x := 5
+	// Implementacion de funcion anonima
+	/* y := func() int {
+		return x * 2
+	}()
+	fmt.Println(y) */
+
+	c := make(chan int)
+	go func() {
+		fmt.Println("Starting function")
+		time.Sleep(5 * time.Second)
+		fmt.Println("End")
+		c <- 1
+	}()
+	<-c
+
+}
